23f2027f317891043c186260a4bc5e486d0a53ac
Iwona Michalak <iwmichalak@interia.pl>
2018-03-11 15:53:07 +0100

    zmiany Iwona

--- src/main/java/pl/sda/clinic/controller/PatientController.java
+++ src/main/java/pl/sda/clinic/controller/PatientController.java
@@ -1,4 +1,9 @@
 package pl.sda.clinic.controller;
 
+import pl.sda.clinic.model.Patient;
+
+import java.util.List;
+
 public class PatientController {
+ //   List<Patient> Patients = new ArrayList<>();
 }
