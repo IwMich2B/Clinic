8c1bca117dab494b0de87b87e28ae9d70af12718
Iwona Michalak <iwmichalak@interia.pl>
2018-03-11 11:52:43 +0100

    classa App

---
+++ src/main/java/pl/sda/clinic/App.java
@@ -0,0 +1,21 @@
+package pl.sda.clinic;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+@ComponentScan("pl.sda.clinic")
+
+public class App {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class, args);
+
+    }
+
+}
