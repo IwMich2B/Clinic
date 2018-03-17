fe83f207ee6100e1d0201198d14bf591a8ef5273
Witold Mrózek <wmrozek89@gmail.com>
2018-03-17 12:32:34 +0100

    Poprawki dla mastera.

--- src/main/java/pl/sda/clinic/model/Patient.java
+++ src/main/java/pl/sda/clinic/model/Patient.java
@@ -1,9 +1,7 @@
 package pl.sda.clinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
--- src/main/java/pl/sda/clinic/services/PatientService.java
+++ src/main/java/pl/sda/clinic/services/PatientService.java
@@ -2,9 +2,9 @@ package pl.sda.clinic.services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+import pl.sda.clinic.PatientRepository;
 import pl.sda.clinic.dto.LoginForm;
 import pl.sda.clinic.model.Patient;
-import pl.sda.clinic.repository.PatientRepository;
 
 import javax.annotation.Resource;
 
@@ -15,7 +15,7 @@ public class PatientService {
     private PatientRepository patientRepository;
 
     public Patient login(LoginForm loginForm){
-    Patient patient = patientRepository.findPatientByLoginAndPasssword(loginForm.getLogin(), loginForm.getPassword());
+    Patient patient = patientRepository.findPatientByLoginAndPassword(loginForm.getLogin(), loginForm.getPassword());
     return patient;
 }
 
