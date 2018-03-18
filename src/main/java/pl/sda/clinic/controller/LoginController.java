5c9651572126cdc4e67230789f012ed9d1371f0f
Anna Gajewska <hikora@wp.pl>
2018-03-18 11:46:50 +0100

    login/specialization

--- src/main/java/pl/sda/clinic/controller/LoginController.java
+++ src/main/java/pl/sda/clinic/controller/LoginController.java
@@ -1,5 +1,4 @@
 package pl.sda.clinic.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,23 +9,17 @@ import org.springframework.web.servlet.ModelAndView;
 import pl.sda.clinic.dto.LoginForm;
 import pl.sda.clinic.model.Patient;
 import pl.sda.clinic.services.PatientService;
-import pl.sda.clinic.utils.LogSession;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
+
 
 @Controller
 public class LoginController {
 
     @Autowired
     PatientService patientService;
-
-    @Autowired
-    LogSession logSession;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage(Patient patient){
@@ -43,14 +36,10 @@ public class LoginController {
         if (patient != null) {
             //Otwieramy sesję użytkownika
             HttpSession session = request.getSession();
-            session.setAttribute("patient", patient.getLogin());
-            //Ustawiamy czas trwania sesji za pomocą ciasteczka
-            Cookie cookie = new Cookie("session-time", "");
-            cookie.setMaxAge(1200);
-            response.addCookie(cookie);
-            logSession.checkSession(request, response);
+            session.setAttribute("patientLogin", patient.getLogin());
+            session.setAttribute("patientId", patient.getId());
 
-            return "redirect:./wyszukiwarka";
+            return "redirect:./specialization";
         }else{
             return "redirect:./login?error";
         }
